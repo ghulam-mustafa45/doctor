489328a4488caa1d41b4f2671d8aac05fe363677
ghulam-mustafa45 <139062950+ghulam-mustafa45@users.noreply.github.com>
2023-10-18 15:38:49 +0500

    changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,12 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Main, NextScript } from 'next/document'
+import Head from 'next/head';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <title >Bernie Yazzie Consulting</title >
+      </Head>
       <body>
         <Main />
         <NextScript />
